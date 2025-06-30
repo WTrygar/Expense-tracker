8b234606f200e99d114cbb99376394e1c0b6ffe3
WTrygar <chandelier.wt@gmail.com>
2025-06-30 11:15:50 +0200

    feat: flat transaction in TransactionList component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <Header/>
+  <Header />
   <div class="container">
-    <Balance/>
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
+import Header from "./components/Header.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
 export default {
   components: {
     Header,
     Balance,
+    IncomeExpenses,
+    TransactionList,
   },
 };
 </script>
