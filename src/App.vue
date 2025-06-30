5dc5246b00c1ff24fe6a79580d035d0da1729543
WTrygar <chandelier.wt@gmail.com>
2025-06-30 11:08:31 +0200

    feat: added flat Balance

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <Header />
+  <Header/>
+  <div class="container">
+    <Balance/>
+  </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
 export default {
   components: {
     Header,
+    Balance,
   },
 };
 </script>
