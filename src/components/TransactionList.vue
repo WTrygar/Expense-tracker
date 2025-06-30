efd44ad8b1f0b894df7b5a0c14cdfc235d218e7e
WTrygar <chandelier.wt@gmail.com>
2025-06-30 14:19:56 +0200

    feat: toasty working delete button

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -6,7 +6,9 @@
       :key="transaction.id"
       :class="transaction.amount < 0 ? 'minus' : 'plus'"
       >{{ transaction.text }} <span>$ {{ transaction.amount }}</span
-      ><button class="delete-btn">x</button></li
+      ><button @click="deleteTransaction(transaction.id)" class="delete-btn"
+        >x</button
+      ></li
     >
   </ul>
 </template>
@@ -14,10 +16,16 @@
 <script setup>
 import { defineProps } from "vue";
 
+const emit = defineEmits(["transactionDeleted"]);
+
 const props = defineProps({
   transactions: {
     type: Array,
     required: true,
   },
 });
+
+const deleteTransaction = (id) => {
+  emit("transactionDeleted", id);
+};
 </script>
